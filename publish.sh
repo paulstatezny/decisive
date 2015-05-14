2c8c6dba3861f8074ba45c6f45dff24cea03539f
Paul Statezny <paulstatezny@users.noreply.github.com>
2015-05-14 08:26:03 -0700

    Refs #19 - Finish publish script.

--- publish.sh
+++ publish.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-if [ "$(git branch)" != "develop" ]
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "develop" ]
 	then
 		echo "Not on develop. Exiting..."
 		exit 1
 fi
 
+# Build the JavaScript app
+printf "\nBUILDING APP\n";
 npm run build;
-rm !(build);
+
+# Zip the built files
+printf "\nBUNDLING FILES\n";
+zip -r build build;
+
+# Move built files to root directory
+printf "\nMOVING FILES TO gh-pages BRANCH\n";
+git checkout gh-pages
+rm -rf build/
+unzip build
+rm build.zip
+
+# Move built files to root directory
+ls | grep -v build | grep -v node_modules | grep -v .gitignore | xargs rm -rf;
+mv build/*.* ./
+rm -rf build/
