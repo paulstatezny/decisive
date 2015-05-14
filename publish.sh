bd7d5e6601ac3410393803840521a9c9f8196ab8
Paul Statezny <paulstatezny@users.noreply.github.com>
2015-05-14 08:39:36 -0700

    Update publish script.

--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$(git rev-parse --abbrev-ref HEAD)" != "develop" ]
-	then
-		echo "Not on develop. Exiting..."
-		exit 1
+    then
+        echo "Not on develop. Exiting..."
+        exit 1
 fi
 
 # Build the JavaScript app
@@ -11,11 +11,11 @@ printf "\nBUILDING APP\n";
 npm run build;
 
 # Zip the built files
-printf "\nBUNDLING FILES\n";
+printf "\nBUNDLING FILES\n\n";
 zip -r build build;
 
 # Move built files to root directory
-printf "\nMOVING FILES TO gh-pages BRANCH\n";
+printf "\nCOMMITTING FILES TO gh-pages BRANCH\n\n";
 git checkout gh-pages
 rm -rf build/
 unzip build
@@ -25,3 +25,17 @@ rm build.zip
 ls | grep -v build | grep -v node_modules | grep -v .gitignore | xargs rm -rf;
 mv build/*.* ./
 rm -rf build/
+
+# Push files to GitHub
+git add -A
+git commit --amend --no-edit
+
+printf "\nSENDING TO GITHUB\n\n";
+
+git push --force origin
+
+printf "\nHEADED BACK TO DEVELOP\n\n";
+
+git checkout develop
+
+printf "\nDONE\n\n";
