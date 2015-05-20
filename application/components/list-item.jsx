3f736e239dcff8222ca2a59b677ff2803f4686c3
Adam Gerbens <adam@synapsestudios.com>
2015-05-20 00:13:05 -0700

    refs #21 | Making drag and drop work without being very elegant in the design

--- application/components/list-item.jsx
+++ application/components/list-item.jsx
@@ -1,22 +1,47 @@
 'use strict';
 
 var React = require('react');
+var ItemTypes = require('./constants').ItemTypes;
+var DragSource = require('react-dnd').DragSource;
 
-module.exports = React.createClass({
+var listItemSource = {
+  beginDrag: function (props, monitor) {
+    return {
+        index        : props.index,
+        task         : props.task,
+        dragQuadrant : props.quadrant
+    };
+  }
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  };
+}
+
+var ListItem = React.createClass({
     displayName : 'ListItem',
 
     propTypes : {
-        task            : React.PropTypes.string.isRequired,
-        completed       : React.PropTypes.bool.isRequired,
-        removeItem      : React.PropTypes.func.isRequired,
-        toggleCompleted : React.PropTypes.func.isRequired
+        index             : React.PropTypes.number.isRequired,
+        quadrant          : React.PropTypes.string.isRequired,
+        task              : React.PropTypes.string.isRequired,
+        completed         : React.PropTypes.bool.isRequired,
+        removeItem        : React.PropTypes.func.isRequired,
+        toggleCompleted   : React.PropTypes.func.isRequired,
+        connectDragSource : React.PropTypes.func.isRequired,
+        isDragging        : React.PropTypes.bool.isRequired
     },
 
     render : function()
     {
         var classes = this.props.completed ? 'checklist__item--checked' : '';
+        var connectDragSource = this.props.connectDragSource;
+        var isDragging = this.props.isDragging;
 
-        return (
+        return connectDragSource(
             <li className={classes}>
                 <label>
                     <input type='checkbox' checked={this.props.completed} onClick={this.props.toggleCompleted} />
@@ -27,3 +52,5 @@ module.exports = React.createClass({
         );
     }
 });
+
+module.exports = new DragSource(ItemTypes.LISTITEM, listItemSource, collect)(ListItem);
--- application/components/quadrant.jsx
+++ application/components/quadrant.jsx
@@ -3,16 +3,34 @@
 var _        = require('underscore');
 var React    = require('react');
 var ListItem = require('./list-item');
+var ItemTypes = require('./constants').ItemTypes;
+var DropTarget = require('react-dnd').DropTarget;
 
-module.exports = React.createClass({
+var quadrantTarget = {
+  drop: function (props, monitor) {
+    var item = monitor.getItem();
+    props.moveTask(item.dragQuadrant, item.task, item.index);
+  }
+};
+
+function collect(connect, monitor) {
+    return {
+        connectDropTarget : connect.dropTarget(),
+        isOver            : monitor.isOver()
+    };
+}
+
+var Quadrant = React.createClass({
     displayName : 'Quadrant',
 
     propTypes : {
+        name       : React.PropTypes.string.isRequired,
         className  : React.PropTypes.string.isRequired,
         hint       : React.PropTypes.string.isRequired,
         tasks      : React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
         addTask    : React.PropTypes.func.isRequired,
-        removeTask : React.PropTypes.func.isRequired
+        removeTask : React.PropTypes.func.isRequired,
+        isOver     : React.PropTypes.bool.isRequired
     },
 
     getInitialState : function()
@@ -24,14 +42,17 @@ module.exports = React.createClass({
 
     renderItems : function()
     {
-        var removeTask, toggleCompleted;
+        var removeTask, toggleCompleted, quadrantName;
 
         removeTask      = this.props.removeTask;
         toggleCompleted = this.props.toggleCompleted;
+        quadrantName    = this.props.name;
 
         return this.props.tasks.map(function (item, index) {
             return (
                 <ListItem
+                    index           = {index}
+                    quadrant        = {quadrantName}
                     task            = {item.task}
                     completed       = {item.completed}
                     toggleCompleted = {_.partial(toggleCompleted, index)}
@@ -66,8 +87,16 @@ module.exports = React.createClass({
 
     render : function()
     {
-        return (
-            <div className={'grid__quadrant ' + this.props.className}>
+        var isOver = this.props.isOver,
+            connectDropTarget = this.props.connectDropTarget,
+            divClassName = 'grid__quadrant ' + this.props.className;
+
+        if (isOver) {
+            divClassName += ' grid__quadrant--hover';
+        }
+
+        return connectDropTarget(
+            <div className={divClassName}>
                 <p className='quadtrant__hint'>{this.props.hint}</p>
                 <ul className='checklist'>
                     {this.renderItems()}
@@ -84,6 +113,9 @@ module.exports = React.createClass({
                     </li>
                 </ul>
             </div>
+
         );
     }
 });
+
+module.exports = new DropTarget(ItemTypes.LISTITEM, quadrantTarget, collect)(Quadrant);
