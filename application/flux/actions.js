17e3532712732519eb60bed7425b8a8a7e57eb59
Paul Statezny <paulstatezny@users.noreply.github.com>
2015-06-15 08:16:41 -0700

    Use ES6 to make actions more concise.

--- application/flux/actions.js
+++ application/flux/actions.js
@@ -1,39 +1,27 @@
 'use strict';
 
 module.exports = {
-    addGrid : function(name)
+    addGrid(name)
     {
         this.dispatch('ADD_GRID', name);
     },
 
-    addTask : function(grid, quadrant, task)
+    addTask(grid, quadrant, task)
     {
-        this.dispatch('ADD_TASK', {
-            grid     : grid,
-            quadrant : quadrant,
-            task     : task
-        });
+        this.dispatch('ADD_TASK', {grid, quadrant, task});
     },
 
-    toggleCompleted : function(grid, quadrant, taskId)
+    toggleCompleted(grid, quadrant, id)
     {
-        this.dispatch('TOGGLE_COMPLETED', {
-            grid     : grid,
-            quadrant : quadrant,
-            id       : taskId
-        });
+        this.dispatch('TOGGLE_COMPLETED', {grid, quadrant, id});
     },
 
-    removeTask : function(grid, quadrant, taskId)
+    removeTask(grid, quadrant, id)
     {
-        this.dispatch('REMOVE_TASK', {
-            grid     : grid,
-            quadrant : quadrant,
-            id       : taskId
-        });
+        this.dispatch('REMOVE_TASK', {grid, quadrant, id});
     },
 
-    selectGrid : function(index)
+    selectGrid(index)
     {
         this.dispatch('SELECT_GRID', index);
     }
